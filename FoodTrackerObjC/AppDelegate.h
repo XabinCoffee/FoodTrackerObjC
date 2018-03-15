e01479166c8ef4269383137363261a74ac9475a8
Xabin Rodriguez <xabin95@gmail.com>
2018-03-15 15:30:24 +0100

    Initial Commit

---
+++ FoodTrackerObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FoodTrackerObjC
+//
+//  Created by Xabin Rodriguez on 15/3/18.
+//  Copyright © 2018 Xabin Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FoodTrackerObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FoodTrackerObjC
+//
+//  Created by Xabin Rodriguez on 15/3/18.
+//  Copyright © 2018 Xabin Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
